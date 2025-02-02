41eeed08e066bc5d98a5796496fbf8f381882b2a
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2025-02-02 19:02:36 +0530

    adding more solutions

--- AOC2015/day8/sol2.py
+++ AOC2015/day8/sol2.py
@@ -1,18 +1,9 @@
 def differs_by(x:str)->int:
-    original = len(x)
-    in_mem = len(x)-2
-    i = 1
-    while i < len(x)-1:
-        if x[i] == "\\":
-            if x[i+1] == "x":
-                in_mem -= 3
-                i += 3
-            else:
-                in_mem -= 1
-                i += 1
-        i += 1
-    print(x, "differs_by", original - in_mem)
-    return original - in_mem
+    difference = 2
+    for i in x:
+        if i in ("\\","\""):
+            difference += 1
+    return difference
 with open("input.txt") as f:
     input = f.readlines()
 
---
+++ AOC2015/day9/sol2.py
@@ -0,0 +1,64 @@
+def connect():
+    global cost
+    max = graph["Tristram"]["AlphaCentauri"]
+    city1 = "Tristram"
+    city2 = "AlphaCentauri"
+    for i in cities:
+        for j in cities:
+            if i != j and max < graph[i][j]:
+                city1 = i
+                city2 = j 
+                max = graph[i][j]
+    if cities[city1] + cities[city2] < 2:
+        print(city1 + " to " + city2 + " takes", graph[city1][city2])
+        cities[city1] += 1
+        cities[city2] += 1
+        cost += max 
+    graph[city1][city2] = 0
+    graph[city2][city1] = 0
+
+def fill_values(x:str):
+    processed = x.split(" = ")
+    city1, city2 = processed[0].split(" to ")
+    if city1 not in graph:
+        graph[city1] = {}
+    if city2 not in graph:
+        graph[city2] = {}
+    graph[city1][city2] = int(processed[1])
+    graph[city2][city1] = int(processed[1])
+
+with open("input.txt","r") as f:
+    input = f.readlines()
+
+cities = {
+    "Tristram" : 0,
+    "AlphaCentauri" : 0,
+    "Snowdin": 0,
+    "Tambi": 0,
+    "Faerun": 0,
+    "Norrath": 0,
+    "Straylight":0,
+    "Arbre": 0,
+}
+
+graph = {}
+cost = 0
+for i in input:
+    fill_values(i)
+
+for i in graph:
+    print(i ,"-> ", graph[i])
+
+while True:
+    ones = 0
+    twos = 0
+    for i in cities:
+        if cities[i] == 1:
+            ones += 1
+        if cities[i] == 2:
+            twos += 1
+    if ones == 2 and twos == 6:
+        break
+    connect()
+
+print(cost)
